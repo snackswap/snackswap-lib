5f546e8556af9ce55a085caf7208ab59af898066
imbilly2510 <hi.imbilly2510@gmail.com>
2021-03-12 17:08:24 +0700

    yarn

--- test/PairNamer.spec.ts
+++ test/PairNamer.spec.ts
@@ -34,8 +34,8 @@ describe('PairNamer', () => {
     }
 
     it('concatenation', async () => {
-      expect(await pairName('DAI Stable Coin', 'MKR Token', 'Uniswap V2 Liquidity Provider Share for ', '.')).to.eq(
-        'Uniswap V2 Liquidity Provider Share for DAI Stable Coin:MKR Token.'
+      expect(await pairName('DAI Stable Coin', 'MKR Token', 'Snackswap Liquidity Provider Share for ', '.')).to.eq(
+        'Snackswap Liquidity Provider Share for DAI Stable Coin:MKR Token.'
       )
     })
   })
